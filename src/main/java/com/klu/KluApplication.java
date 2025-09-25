314c241d1a4d22be8cebeb8be0ce76d9dc4c0166
INKOLLU-THARUN <2300032574@kluniversity.in>
2025-09-25 12:50:59 +0530

    updated backend

--- src/main/java/com/klu/KluApplication.java
+++ src/main/java/com/klu/KluApplication.java
@@ -2,9 +2,10 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class KluApplication {
+public class KluApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KluApplication.class, args);
